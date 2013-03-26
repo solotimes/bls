c82cedc97328729e41294fc32cc147f66f3e8711
solotimes <toknight@gmail.com>
2013-03-26 14:02:03 +0800

    修正编辑器对话框默认位置

---
+++ public/jmeditor/ckeditor/config.js
@@ -0,0 +1,44 @@
+/**
+ * @license Copyright (c) 2003-2012, CKSource - Frederico Knabben. All rights reserved.
+ * For licensing, see LICENSE.html or http://ckeditor.com/license
+ */
+
+CKEDITOR.editorConfig = function( config ) {
+  // Define changes to default configuration here.
+  // For the complete reference:
+  // http://docs.ckeditor.com/#!/api/CKEDITOR.config
+
+  // The toolbar groups arrangement, optimized for two toolbar rows.
+  config.toolbarGroups = [
+    // { name: 'clipboard',   groups: [ 'clipboard', 'undo' ] },
+    // { name: 'editing',     groups: [ 'find', 'selection', 'spellchecker' ] },
+    // { name: 'links' },
+    { name: 'forms' },
+    { name: 'tools' },
+    // { name: 'document',     groups: [ 'mode', 'document', 'doctools' ] },
+    { name: 'others' },
+    '/',
+    { name: 'basicstyles', groups: [ 'basicstyles', 'cleanup' ] },
+    // { name: 'paragraph',   groups: [ 'list', 'indent', 'blocks', 'align' ] },
+    { name: 'insert' }
+    //{ name: 'styles' },
+    //{ name: 'colors' },
+    // { name: 'about' }
+  ];
+
+  // Remove some buttons, provided by the standard plugins, which we don't
+  // need to have in the Standard(s) toolbar.
+  config.removeButtons = 'Underline,Subscript,Superscript,SpecialChar,Table,HorizontalRule';
+  config.extraPlugins = 'jme';
+  config.dialog_backgroundCoverOpacity = 0;
+  // config.enterMode = CKEDITOR.ENTER_BR;
+};
+
+CKEDITOR.on('dialogDefinition', function(e) {
+    var dialogName = e.data.name;
+    var dialogDefinition = e.data.definition;
+    dialogDefinition.onShow = function() {
+      var x = $(window).width()*3/4 - this.getSize().width/2;
+      this.move(x,0); // Top center
+    };
+})
