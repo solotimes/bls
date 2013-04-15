b803eb0d0205991980aa25b823e635791185c493
solotimes <toknight@gmail.com>
2013-04-15 17:25:34 +0800

    试卷库报告

--- assets/js/paper/app.js
+++ assets/js/paper/app.js
@@ -4,5 +4,6 @@ var app = angular.module('Paper', ['paper.filters', 'paper.services', 'paper.dir
     $routeProvider.when('/marking', {templateUrl: '/templates/marking.html', controller: 'MarkingCtrl'});
     $routeProvider.when('/edit', {templateUrl: '/templates/edit.html', controller: 'PaperCtrl'});
     $routeProvider.when('/report', {templateUrl: '/templates/report.html', controller: 'ReportCtrl'});
+    $routeProvider.when('/paper-report', {templateUrl: '/templates/paper-report.html', controller: 'PaperReportCtrl'});
     $routeProvider.otherwise({templateUrl: '/templates/view.html', controller: 'PaperCtrl'});
   }]);
--- assets/js/paper/controllers/report.js
+++ assets/js/paper/controllers/report.js
@@ -27,8 +27,49 @@ app.controller('ReportCtrl',['$scope','$http','paper','knowledgeTree','modal',fu
     });
   });
 
+}]);
+
+app.controller('PaperReportCtrl',['$scope','$http','paper','knowledgeTree','modal',function(scope,http,paper,knowledgeTree){
+  // scope.$watch('paper.questions',function(questions){
+    scope.kids = [];
+    if(paper.questions){
+      paper.questions.forEach(function(q){
+        if(q.knowledges)
+          q.knowledges.forEach(function(k){
+            if(scope.kids.indexOf(k.id) == -1){
+              scope.kids.push(k.id);
+              scope.counts = scope.counts || {};
+              scope.counts[k.id] = scope.counts[k.id] || [0,0,0];
+              scope.counts[k.id][q.Type]++;
+            }
+          });
+      });
+    }
+
+  scope.knowledgeTree = knowledgeTree;
+
+  knowledgeTree.load().then(function(){
+    // console.log(knowledgeTree.knowledges);
+    // console.log(scope.kids);
+    scope.knowledges = knowledgeTree.knowledges.filter(function(knowledge){
+      if(scope.kids.indexOf(knowledge.id) != -1){
+        knowledge.counts = scope.counts[knowledge.id];
+        return true;
+      }
+      return false;
+    });
+    console.log(scope.knowledges);
+    // scope.rows.forEach(function(row){
+    //   var questions = paper.getQuestionsByKid(row.knowledge.id);
+    //   var wrongQuestions = questions.filter(function(q){return q.Wrong;});
+    //   row[4] = wrongQuestions.length;
+    //   row[3] = Math.round(100* row[4] / questions.length);
+    //   row[5] = wrongQuestions;
+    // });
+  });
 
 }]);
+
 app.controller('CustomerReportCtrl',['$scope','$http','paper','knowledgeTree','modal',function(scope,http,paper,knowledgeTree,modal){
   // scope.$watch('paper.questions',function(questions){
   var report = window.report;
