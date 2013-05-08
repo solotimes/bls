bd2aa26f91d718600aff4774198ed06ab7eda079
solotimes <toknight@gmail.com>
2013-05-08 09:51:16 +0800

    #223431 录入的时候：知识点保留上一题的知识点（新需求）

--- assets/js/paper/controllers/recording.js
+++ assets/js/paper/controllers/recording.js
@@ -48,8 +48,10 @@ app.controller('RecordingCtrl',['$scope','$http','paper','$location',function(sc
 
   scope.$watch('question',function(currentQuestion){
     var prevType = currentQuestion && currentQuestion.Order && paper.questions[currentQuestion.Order-1].Type;
+    var prevKnowledges = currentQuestion && currentQuestion.Order && paper.questions[currentQuestion.Order-1].knowledges;
     if(currentQuestion && !currentQuestion.id){
       currentQuestion.Type = prevType || 0; //若问题为空,使用上题的类型
+      currentQuestion.knowledges = prevKnowledges || []; //若问题为空,使用上题的知识点
     }
   });
 
